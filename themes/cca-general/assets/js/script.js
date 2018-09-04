4978e76879a948d0d1595853f6b1ae24eb2542a6
Khosrow Moossavi <khos2ow@gmail.com>
2018-09-04 17:25:40 -0400

    Reformat code to follow hugo standards (#12)
    * Upgrade hugow to v1.1.0
    
    * Upgrade hugo to v0.47.1
    
    * Reformat code to follow hugo standards

---
+++ themes/cca-general/assets/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    initNavBarSlideOut();
+    initScrollForNavBar();
+});
+
+var initNavBarSlideOut = function () {
+    // Open navbarSide when button is clicked
+    $('#navbar-side-button').on('click', function () {
+        $('.navbar-side').addClass('reveal');
+        $('.overlay').show();
+    });
+
+    // Close navbarSide when the outside of menu is clicked
+    $('.overlay').on('click', function () {
+        $('.navbar-side').removeClass('reveal');
+        $('.overlay').hide();
+    });
+
+    // Close navbarSide when "close" button is pressed
+    $('.close-side-button').on('click', function () {
+        $('.navbar-side').removeClass('reveal');
+        $('.overlay').hide();
+    });
+};
